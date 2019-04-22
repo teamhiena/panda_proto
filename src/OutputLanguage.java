dd4bc143e40d331bc7560fbce8c04c0e4c01f0bb
gombb <gombazsi>
2019-04-22 18:03:33 +0200

    legalabb lefut

--- src/OutputLanguage.java
+++ src/OutputLanguage.java
@@ -1,51 +1,54 @@
 //Kene valami cucc(pl. mainben) ami meghivja majd ennek az osztalynak a megfelelo writeOutput-jat.
 public class OutputLanguage {
+
+    //TODO VALAMI TAGVÁLTOZÓ AZ OUTPUTRA
+
     public void writeTile(Tile t){
-        System.out.println("stats of: "+t.toString()+"\n"
-                +"entity: "+t.getEntity().toString()+"\n"
-                +"animal: "+t.getAnimal().toString()+"\n"
+        System.out.println("stats of: "+t+"\n"
+                +"entity: "+t.getEntity()+"\n"
+                +"animal: "+t.getAnimal()+"\n"
                 +"subbedPandas: "+t.writeSubbedPandas()+"\n"
                 +"neighbors: "+t.writeNeighbors()+"\n");
     }
-    public void writeTile(WeakTile t){
-        System.out.println("stats of: "+t.toString()+"\n"
-                +"entity: "+t.getEntity().toString()+"\n"
-                +"animal: "+t.getAnimal().toString()+"\n"
+    public void writeWeakTile(WeakTile t){
+        System.out.println("stats of: "+t+"\n"
+                +"entity: "+t.getEntity()+"\n"
+                +"animal: "+t.getAnimal()+"\n"
                 +"subbedPandas: "+t.writeSubbedPandas()+"\n"
                 +"neighbors: "+t.writeNeighbors()+"\n"
                 +"numOfSteps: "+t.getNumOfSteps()+"\n"
                 +"isBroken: "+ Boolean.toString(t.isBroken()));
     }
     public void writeOrangutan(Orangutan o){
-        System.out.println("stats of: "+o.toString()+"\n"
-                +"tile: "+o.getTile().toString()+"\n"
-                +"nextTile: "+o.getNextTile().toString()+"\n"
-                +"followedBy: "+o.getFollowedBy().toString()+"\n"
-                +"following: "+o.getFollowing().toString()+"\n"
+        System.out.println("stats of: "+o+"\n"
+                +"tile: "+o.getTile()+"\n"
+                +"nextTile: "+o.getNextTile()+"\n"
+                +"followedBy: "+o.getFollowedBy()+"\n"
+                +"following: "+o.getFollowing()+"\n"
                 +"score: "+o.getScore());
     }
     public void writePanda(Panda p)
     {
-        System.out.println("stats of: "+p.toString()+"\n"
-                +"tile: "+p.getTile().toString()+"\n"
-                +"nextTile: "+p.getNextTile().toString()+"\n"
-                +"followedBy: "+p.getFollowedBy().toString()+"\n"
-                +"following: "+p.getFollowing().toString());
+        System.out.println("stats of: "+p+"\n"
+                +"tile: "+p.getTile()+"\n"
+                +"nextTile: "+p.getNextTile()+"\n"
+                +"followedBy: "+p.getFollowedBy()+"\n"
+                +"following: "+p.getFollowing());
     }
     public void writeNonEnterableEntity(NonEnterableEntity e){
-        System.out.println("stats of: "+e.toString()+"\n"
-                +"tile: "+e.getTile().toString());
+        System.out.println("stats of: "+e+"\n"
+                +"tile: "+e.getTile());
     }
     public void writeFotel(Fotel f){
-        System.out.println("stats of: "+f.toString()+"\n"
-                +"tile: "+f.getTile().toString()+"\n"
-                +"enteredFrom: "+f.getEnteredFrom().toString()+"\n"
+        System.out.println("stats of: "+f+"\n"
+                +"tile: "+f.getTile()+"\n"
+                +"enteredFrom: "+f.getEnteredFrom()+"\n"
                 +"timeleft: "+f.getTimeLeft());
     }
     public void writeWardrobe(Wardrobe w){
-        System.out.println("stats of: "+w.toString()+"\n"
-                +"tile: "+w.getTile().toString()+"\n"
-                +"previousExitTile: "+w.getPreviousExitTile().toString()+"\n"
+        System.out.println("stats of: "+w+"\n"
+                +"tile: "+w.getTile()+"\n"
+                +"previousExitTile: "+w.getPreviousExitTile()+"\n"
                 +"entrance: "+w.getEntrance());
     }
 }
