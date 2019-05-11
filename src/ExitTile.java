0890d20f427cca90d6d17ce7099370cc71547c06
Balázs Paréj <booneyka@live.com>
2019-05-11 13:58:54 +0200

    exit

--- src/ExitTile.java
+++ src/ExitTile.java
@@ -23,14 +23,23 @@ public class ExitTile extends Tile {
             //mi tortenik
             return false;
         }
-
-        boolean success=false;
-        if(entity!=null)//ha van ott entiy akk megprobalok belelepni
-            success=entity.stepIn(o); //ha nem enterable vagy panda ul benne akk false
-        else if(animal!=null) {
-            success=animal.getCaughtBy(o);
+        boolean success=true;
+        if(entity != null)//Ha van ott entiy akk megprobalok belelepni. (ami nem fotel az return false)
+            success = entity.stepIn(o);
+        else if(animal != null && o.getStepCounter() >= 4) {
+            success = animal.getCaughtBy(o);
         }
-        return false;
+
+        if(success) {
+            this.setAnimal(o);
+            //o.getTile().setAnimal(o.followedBy); GOMBA szombat 12:18 szerintem ez kurja el a stepeket
+            o.getTile().setAnimal(null); //GOMBA szombat 12:24 ez a fix
+            o.setTile(this);
+        }
+
+        //Nincs ott allat de olyan entity van amibe (most) nem lehet belelepni
+        //pl nonenterableentity vagy egy hasznalatban levo fotel
+        return success;
     }
     @Override
     public boolean receiveAnimal(Panda p) {
@@ -47,10 +56,9 @@ public class ExitTile extends Tile {
             success = entity.stepIn(p);
         }
         if (success) {
-            if(p.followedBy!=null)
-                p.followedBy.setNextTile(p.tile);
             this.setAnimal(p);
-            p.getTile().setAnimal(null);
+            if(p.getTile().getAnimal()==p)
+                p.getTile().setAnimal(null);//lehet hogy elkap egy orangutan es akkor az mar ott van, nem kell kinullazni
             p.setTile(this);
         }
         return success;
